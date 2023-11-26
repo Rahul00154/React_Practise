38063df1480975a87963c43092a4ef35ba05645a
Rahul00154 <103448932+Rahul00154@users.noreply.github.com>
2023-11-27 02:54:26 +0530

    half done

---
+++ Contact_App/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <div className=" flex justify-center items-center bg-white h-[60px] m-4 rounded-md">
+      <img src="/logos_firebase.svg" />
+      <h1>FireBase Contact App</h1>
+    </div>
+  );
+}
+
+export default Navbar;
