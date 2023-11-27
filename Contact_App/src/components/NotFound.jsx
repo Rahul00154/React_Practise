ac9b6f348c748731a8d61ba9463bd72590dac00d
Rahul00154 <103448932+Rahul00154@users.noreply.github.com>
2023-11-27 21:35:26 +0530

    done

---
+++ Contact_App/src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div>
+      <img src="/Hands Contact.png" alt="" />
+      <h1>Contact Not Found</h1>
+    </div>
+  );
+}
+
+export default NotFound;
